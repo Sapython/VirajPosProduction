8371d87ab948e3fe7b384a82865019d911c01c61
Kumar Saptam <45693173+Sapython@users.noreply.github.com>
2023-05-11 17:05:01 +0530

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -99,13 +99,14 @@ function createWindow(): BrowserWindow {
 
     const url = new URL(path.join('file:', __dirname, pathIndex));
     win.loadURL(url.href);
+    win.webContents.on("did-fail-load", () => {
+      console.log("did-fail-load");
+      const url = new URL(path.join('file:', __dirname, pathIndex));
+      win.loadURL(url.href);
+      // REDIRECT TO FIRST WEBPAGE AGAIN
+    });
   }
-  win.webContents.on("did-fail-load", () => {
-    console.log("did-fail-load");
-    win.loadURL(path.join("file://", __dirname, "../dist/index.html"));
-    // REDIRECT TO FIRST WEBPAGE AGAIN
-  });
-
+  
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store window

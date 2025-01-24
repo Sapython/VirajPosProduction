36d51d5bd7ea177a1cade1102764899746a006d2
Saptam <saptampro2003@gmail.com>
2025-01-24 23:49:24 +0530

    refactor: simplify guard classes and update Angular dependencies to version 16

--- src/app/shared/guards/auth-guard.guard.ts
+++ src/app/shared/guards/auth-guard.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DataProvider } from '../../core/services/provider/data-provider.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private dataProvider: DataProvider,
     private router: Router,
--- src/app/shared/guards/login.guard.ts
+++ src/app/shared/guards/login.guard.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DataProvider } from '../../core/services/provider/data-provider.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
   constructor(private dataProvider: DataProvider) {}
   canActivate(
     route: ActivatedRouteSnapshot,
